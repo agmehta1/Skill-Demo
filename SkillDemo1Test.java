08e2772119014392356ea58e7c811ddd9d7625c5
agmehta1 <agmehta@ucsd.edu>
2022-02-04 15:34:35 -0800

    Update SkillDemo1Test.java

--- SkillDemo1Test.java
+++ SkillDemo1Test.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemo1Test {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemo1.add(1,3));
+        assertEquals(2, SkillDemo1.add(1,1));
     }
 }
