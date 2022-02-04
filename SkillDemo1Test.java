c857d8c7fc0829716f57e25712aa529f7c1d2ff6
agmehta1 <agmehta@ucsd.edu>
2022-02-04 15:32:00 -0800

    add junit and broken code

---
+++ SkillDemo1Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+ 
+public class SkillDemo1Test {
+    @Test
+    public void addition() {
+        assertEquals(2, SkillDemo1.add(1,3));
+    }
+}
